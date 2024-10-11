c04c64fc3df970d93ea5220b6bfa6d162d1b2cdf
sasukee11-max <jphnewslettery@proton.me>
2024-10-11 18:02:03 +0200

    initial

---
+++ src/main/java/com/example/strategy/entity/Person.java
@@ -0,0 +1,20 @@
+package com.example.strategy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
